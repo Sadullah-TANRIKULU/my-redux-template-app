8fd59915239f87e7d2b20e89e148d74729b79b56
Sadullah-TANRIKULU <sadullahtanrikulu@gmail.com>
2023-04-22 18:57:56 +0300

    last changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Note from './components/Note';
 const App = () => {
   
   return (
-    <div className='m-1 bg-slate-300' >
-      <h1 className='w-full text-center text-4xl mb-3' >Daily Notes</h1>
+    <div className='bg-slate-300 flex flex-col items-center w-full' >
+      <h1 className='w-full h-16 text-center text-4xl p-2 fixed bg-slate-300 z-10' >Daily Notes</h1>
       <Note />
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteNote, updateNote, addNote } from "../redux/notesSlice";
+import { TfiPencilAlt, TfiTrash } from "react-icons/tfi";
 
 const Note = () => {
 
@@ -43,44 +44,41 @@ const Note = () => {
     setIsUpdate(false);
   }
 
-
   return (
-    <section className='mt-8 bg-blue-500' >
+    <section className='mt-8 min-h-screen w-11/12' >
 
       {!isUpdate ?
-        <form onSubmit={handleAddSubmit} className='flex flex-col gap-3' >
-          <input type="text" className='rounded-md focus:outline-none indent-2' value={title} ref={titleInputEl} onChange={e => setTitle(e.target.value)} />
-          <input type="text" className='rounded-md focus:outline-none indent-2' value={note} onChange={e => setNote(e.target.value)} />
-          <button type="submit" className='bg-lime-700 rounded-e-md pr-3 pl-3' >Add Note</button>
+        <form onSubmit={handleAddSubmit} className='flex flex-col items-center bg-fuchsia-700 gap-3 w-11/12 rounded-b-md pt-2 fixed top-16' >
+          <input type="text" className='rounded-md focus:outline-none indent-2 mt-1 w-11/12 h-10' value={title} ref={titleInputEl} onChange={e => setTitle(e.target.value)} />
+          <input type="text" className='rounded-md focus:outline-none indent-2 mt-1 w-11/12 h-10' value={note} onChange={e => setNote(e.target.value)} />
+          <button type="submit" className='focus:outline-none hover:bg-green-700 font-medium rounded-lg text-lg px-5 py-2.5 mr-2 mb-2' >Add Note</button>
         </form>
         :
-        <form onSubmit={handleUpdateSubmit} className='flex flex-col gap-3' >
-          <input type="text" className='rounded-md focus:outline-none indent-2' value={title} onChange={e => setTitle(e.target.value)} />
-          <input type="text" className='rounded-md focus:outline-none indent-2' value={note} onChange={e => setNote(e.target.value)} />
-          <button type="submit" className='bg-lime-700 rounded-e-md pr-3 pl-3' >Update Note</button>
+        <form onSubmit={handleUpdateSubmit} className='flex flex-col items-center bg-fuchsia-700 gap-3 w-full rounded-b-md pt-2' >
+          <input type="text" className='rounded-md focus:outline-none indent-2 mt-1 w-11/12 h-10' value={title} onChange={e => setTitle(e.target.value)} />
+          <input type="text" className='rounded-md focus:outline-none indent-2 mt-1 w-11/12 h-10' value={note} onChange={e => setNote(e.target.value)} />
+          <button type="submit" className='focus:outline-none hover:bg-green-700 font-medium rounded-lg text-lg px-5 py-2.5 mr-2 mb-2' >Update Note</button>
         </form>
       }
-      <div className='bg-white min-h-[30rem] mt-3' >
+      <div className='mt-56' >
         {notes ?
           notes.map((item) => (
-            <div key={item.id} className="bg-yellow-400 flex justify-between gap-3 mt-2 mb-2 p-2 shadow-slate-900 shadow-md rounded-lg">
-              <div className='bg-fuchsia-500 w-1/2' >
-              {/* <p>{item.id}</p> */}
-              <p className='text-sm' >{item.title}</p>
-              <p className='text-lg' >{item.note}</p>
+            <div key={item.id} className="bg-yellow-200 flex justify-between gap-3 mt-2 mb-2 p-2 shadow-slate-900 shadow-md rounded-lg">
+              <div className='w-1/2' >
+                {/* <p>{item.id}</p> */}
+                <p className='text-sm text-ellipsis' >{item.title}</p>
+                <p className='text-lg text-ellipsis' >{item.note}</p>
               </div>
-              <div className='flex gap-5 justify-evenly bg-gray-400 w-1/2' >
-                <button
-                  className='bg-lime-700 rounded-e-md pr-3 pl-3'
+              <div className='flex gap-5 justify-evenly w-1/2 items-center' >
+                <TfiTrash
+                  className='w-16 h-12 pr-3 pl-3 max-h-16 cursor-pointer'
                   onClick={() => dispatch(deleteNote(item.id))}
-                >Delete
-                </button>
-                <button
-                  className='bg-lime-700 rounded-e-md pr-3 pl-3'
+                />
+                <TfiPencilAlt
+                  className='w-16 h-12 pr-3 pl-3 max-h-16 cursor-pointer'
                   onClick={() => handleUpdate(item)}
-                >Update
-                </button>
-              </div>
+                />              
+                </div>
             </div>
           ))
           :
